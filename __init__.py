273aad6323bce597712e833a518db49d43d521af
pantynopants <pantynopants@users.noreply.github.com>
2016-11-23 20:43:42 +0800

    change doc

--- __init__.py
+++ __init__.py
@@ -1,1 +1,3 @@
-from . import( main, models, utils, algorithms, algorithms_edgearr)
+# from . import( main, models, utils, algorithms, algorithms_edgearr)
+
+__all__ = ['models', 'main', 'utils', 'algorithms', 'algorithms_edgearr', 'Vgraph', 'nx', 'data']
